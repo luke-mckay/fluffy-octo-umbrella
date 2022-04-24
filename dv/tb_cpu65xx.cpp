75b5d9ad3d345265f54e84ad2adafa7e618aff7f
Luke E. McKay <32602862+luke-mckay@users.noreply.github.com>
2022-04-24 11:24:09 -0600

    update that starts to run model with loaded binary

--- dv/tb_cpu65xx.cpp
+++ dv/tb_cpu65xx.cpp
@@ -38,10 +38,36 @@ extern "C" {
 
 #define MAX_SIM_TIME 20
 vluint64_t sim_time = 0;
+uint8_t memory[64*1024];
 
+using namespace std;
 
 int main(int argc, char** argv, char** env)
 {
+  cout << "You have entered " << argc
+      << " arguments:" << "\n";
+
+  for (int i = 0; i < argc; ++i)
+  {
+      if (!strncmp(argv[i], "--file=",7))
+      {
+       	FILE *f;
+        f = fopen(argv[i]+7, "r");
+        fread(memory, 1, 64*1024, f);
+        fclose(f);
+      }
+      cout << argv[i] << "\n";
+  }
+
+  for (int i = 0; i < /*64* */1024; i+=16)
+  {
+    printf("%02X %02X %02X %02X ", memory[i], memory[i+1], memory[i+2], memory[i+3]);
+    printf("%02X %02X %02X %02X ", memory[i+4], memory[i+5], memory[i+6], memory[i+7]);
+    printf("%02X %02X %02X %02X ", memory[i+8], memory[i+9], memory[i+10], memory[i+11]);
+    printf("%02X %02X %02X %02X ", memory[i+12], memory[i+13], memory[i+14], memory[i+15]);
+    printf("\n");
+  }
+
   Verilated::mkdir("logs");
 
   const std::unique_ptr<VerilatedContext> contextp{new VerilatedContext};
@@ -83,7 +109,7 @@ int main(int argc, char** argv, char** env)
 
 		//if (clk) handle_monitor(state);
 
-		//chipStatus(state);
+		chipStatus(state);
 		//if (!(cycle % 1000)) printf("%d\n", cycle);
 	};
 
@@ -98,3 +124,4 @@ int main(int argc, char** argv, char** env)
   delete dut;
   exit(EXIT_SUCCESS);
 }
+
